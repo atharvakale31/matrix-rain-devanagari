5bc148e87715aaefc914c93d5ba7f1840ffd0e63
Atharva <atharvakale31@gmail.com>
2021-07-17 21:16:52 +0530

    Custom Rain

--- sketch.js
+++ sketch.js
@@ -1,6 +1,12 @@
 var symbol;
-var symbolSize =16;
+var symbolSize= 16;
 var streams =[];
+var temp=0;
+var rain;
+var gradIntensity=5;
+var redComp=0;
+var greenComp=255;
+var blueComp=70;
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
@@ -13,12 +19,6 @@ function setup() {
     streams.push(stream);
     x += symbolSize;
     }
-  //stream = new Stream();
-  //stream.generateSymbols();
-  //symbol = new Symbole(
-     //width/2, 0,random(5,10)
-  //);
-  //symbol.setToRandomSymbol() ;
   textSize(symbolSize)
   
  
@@ -79,10 +79,32 @@ function Stream ()  {
     }
     
   }
+  document.getElementById("Red").onclick = function() {redComp=document.getElementById("Red").value;};
+    document.getElementById("Green").onclick = function() {greenComp=document.getElementById("Green").value;};
+    document.getElementById("Blue").onclick = function() {blueComp=document.getElementById("Blue").value;};
+    document.getElementById("gradientS").onclick = function() {gradIntensity=document.getElementById("gradientS").value;};
+    document.getElementById("solid").onclick = function() {rain="solid";};
+    document.getElementById("gradient").onclick = function() {rain="gradient";};
   
   this.render = function (){
-    this.symbols.forEach(function(symbol){
-    fill(0,255,70);
+    
+    
+    this.symbols.forEach(function(symbol,index){
+      
+      switch(rain) {
+            case "solid":
+              fill(redComp,greenComp,blueComp);
+              break;
+            case "gradient":
+              
+              fill(redComp*index/gradIntensity,greenComp*index/gradIntensity,blueComp*index/gradIntensity)
+              // fill(index*random(2,10),index*random(2,10),index*random(2,10)); //disco
+              break;
+            default:
+               fill(redComp,greenComp,blueComp); //pure green
+
+          }
+
     text(symbol.value,symbol.x, symbol.y);
     symbol.rain() ;
     symbol.setToRandomSymbol();
